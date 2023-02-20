1254d812064411e9f4a4a344e121a3437f0fdee6
415Rajani <125254213+415Rajani@users.noreply.github.com>
2023-02-19 23:06:17 -0800

    Update strimlit_app.py

--- strimlit_app.py
+++ strimlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice=streamlit.text_input('what fruit like  information about?,'Kiwi')
+fruit_choice=streamlit.text_input('what fruit like  information about?','Kiwi')
 streamlit.write('user_entered',fruit_choice)
                                   
 
